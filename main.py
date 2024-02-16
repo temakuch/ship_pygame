39776ff330298e8664c127ac483d1c7523688fb3
temakuch <ku4eruck2005@gmail.com>
2024-02-16 17:42:55 +0200

    updated and added enemy

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 from ship import Ship
 from bullet import Bullet
-
+from alien import Alien
 
 WIDTH = 1280
 HEIGHT = 720
@@ -13,11 +13,13 @@ class StarWars:
         pygame.init()
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.ship = Ship(self)
+        self.alien = Alien(self)
         self.bullets = pygame.sprite.Group()
 
     def _update_screen(self):
         self.screen.fill(BACKGROUNG)
         self.ship.blit_ship()
+        self.alien.blit_alien()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         pygame.display.flip()
@@ -53,6 +55,7 @@ class StarWars:
         while True:
             self._check_events()
             self.ship.update()
+            self.alien.update_alien()
             self.bullets.update()
             self._update_screen()
 
